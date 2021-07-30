a1fafe81bbee2fc6e0472c8763e49a9286c02865
Szymon <szymon.wiercigroch@gmail.com>
2021-07-30 13:32:59 +0200

    Initial Django commit

---
+++ LibraryProject/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=20)
+    number_of_pages = models.IntegerField()
+    cover = models.ImageField()
+    language = models.CharField(max_length=20)
